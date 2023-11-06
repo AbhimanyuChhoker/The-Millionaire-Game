2e46972887f984d83ad3e75c4cca349c950bfcb3
Abhimanyu Chhoker <abhimanyuchhoker16@gmail.com>
2023-11-06 16:14:12 +0530

    Completed intro function

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ energy = 100
 
 
 def intro():
-    
-    return True
+    print("Welcome to The Millionaire Game!!")
+    print("In this game you will have to earn a million dollars and retire before you are 60 years old.")
+    print("You will get a thoughsand dollars to start the game with. You can choose your careers. Good Luck!")
+    os.system('sleep')
