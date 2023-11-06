f304748e0139538d0332d2c9e4a6f5938e5e3dd6
Abhimanyu Chhoker <abhimanyuchhoker16@gmail.com>
2023-11-06 15:47:54 +0530

    Added basic variables and intro function

---
+++ main.py
@@ -0,0 +1,9 @@
+money = 1000
+health = 100
+happiness = 100
+energy = 100
+
+def intro():
+    #* This function tells users the basics of the game and the rules of the game
+    #TODO: Add sound effects while printing letters 
+    return True
