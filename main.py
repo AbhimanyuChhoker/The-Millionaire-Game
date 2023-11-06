20520ed29d21d6092afdd2fb45c2ee1ec6b7fbf8
Abhimanyu Chhoker <abhimanyuchhoker16@gmail.com>
2023-11-06 16:15:20 +0530

    Initialized careerChooser function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 money = 1000
 happiness = 100
 energy = 100
+career = "jobless"
 
 
 def intro():
@@ -10,3 +11,7 @@ def intro():
     print("In this game you will have to earn a million dollars and retire before you are 60 years old.")
     print("You will get a thoughsand dollars to start the game with. You can choose your careers. Good Luck!")
     os.system('sleep')
+
+
+def careerChooser():
+    #TODO for 7th November: Complete this function
