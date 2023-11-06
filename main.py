26aaa5a40e8c678150a9f6e6ad984cd97b5ee28f
Abhimanyu Chhoker <abhimanyuchhoker16@gmail.com>
2023-11-06 16:11:16 +0530

    Removed unnecesarry imports

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
-import pyautogui as pag
 
 money = 1000
-health = 100
 happiness = 100
 energy = 100
 
 
 def intro():
-    # * This function tells users the basics of the game and the rules of the game
+    
     return True
