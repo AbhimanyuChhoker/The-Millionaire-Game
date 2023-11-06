46d9769119956b39f5a180dd4f8e1401c2f4ced3
Abhimanyu Chhoker <abhimanyuchhoker16@gmail.com>
2023-11-06 16:10:49 +0530

    Formatted files

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import os
+import pyautogui as pag
+
 money = 1000
 health = 100
 happiness = 100
 energy = 100
 
+
 def intro():
-    #* This function tells users the basics of the game and the rules of the game
-    #TODO: Add sound effects while printing letters 
+    # * This function tells users the basics of the game and the rules of the game
     return True
