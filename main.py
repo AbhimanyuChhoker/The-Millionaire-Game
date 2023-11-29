7ce20254e656941c66705b81ea4547c24a1d7a95
Abhimanyu Chhoker <abhimanyuchhoker16@gmail.com>
2023-11-29 16:28:31 +0530

    Completed buy function

--- main.py
+++ main.py
@@ -171,31 +171,37 @@ def book_cab(money, destination):
         )
 
 
-#TODO: To be rewritten
-# def buy(grocery_items, supermarket_items, money):
-#     shop_type = get_user_input(
-#         "Please enter the shop which you want to visit[supermarket, grocery store]",
-#         ["supermarket", "grocery store"],
-#         "str",
-#     )
-#     if shop_type.lower() == "supermarket":
-#         print("You can buy the following items: ")
-#         items = supermarket_items.keys()
-#         price = supermarket_items.values()
-#         for item in items:
-#             print(f"{item.title()}: ${price}")
-#         item = get_user_input("Please enter the item you want to buy:")
-#         money -= supermarket_items[item]
-#         inventory_items[item] += 1
-#     elif shop_type.lower() == "grocery store":
-#         print("You can buy the following items: ")
-#         items = grocery_items.keys()
-#         price = grocery_items.values()
-#         for item in items:
-#             print(f"{item.title()}: ${price}")
-#         item = get_user_input("Please enter the item you want to buy:")
-#         money -= grocery_items[item]
-#         inventory_items[item] += 1
+def buy(grocery_items, supermarket_items, money):
+    while True:
+        shop_type = get_user_input(
+            "Please enter the shop which you want to visit[supermarket, grocery store]",
+            ["supermarket", "grocery store"],
+            "str",
+        )
+        if shop_type.lower() == "supermarket":
+            book_cab(money, "supermarket")
+            print("You can buy the following items: ")
+            options = supermarket_items.keys()
+            price = supermarket_items.values()
+        elif shop_type.lower() == "grocery store":
+            book_cab(money, "grocery_items")
+            print("You can buy the following items:")
+            options = grocery_items.keys()
+            price = grocery_items.values()
+
+        for item in options:
+            print(f"{item.title()}: ${price}")
+        options.append("exit")
+        choice = get_user_input(
+            "Please enter the item you want to buy or exit to leave the shop: ",
+            options,
+            "str",
+        )
+        if choice.lower() == "exit":
+           break
+        else:
+            money -= supermarket_items[choice.lower()]
+            inventory_items[item] += 1
 
 
 def setup_house(selected_house):
@@ -207,7 +213,7 @@ def setup_house(selected_house):
         print("To buy that you will have to go to a shop.")
         book_cab(money, "supermarket")
         buy()
-        #TODO: To be completed
+        # TODO: To be completed
 
 
 # Simulate work by updating the time
